d3d603ff1afdab640ca0f1d5fcf320cc03fbf1c3
Gerardo Cetzal <gerardoce23@gmail.com>
2018-08-27 08:46:07 -0500

    example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Workbook } from 'exceljs';
+import * as fs from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  generateExcel() {
+    
+    //Excel Title, Header, Data
+    const title = 'Reporte Venta ';
+    const header = ["Año", "Mes", "Fabricante", "Modelo", "Cantidad", "Pct"]
+    const dataTable = [
+      [2007, 1, "Volkswagen ", "Volkswagen Passat", 1267, 10],
+      [2007, 1, "Toyota ", "Toyota Rav4", 819, 6.5],
+
+    ];
+
+    //Create workbook and worksheet
+    let workbook = new Workbook();
+    let worksheet = workbook.addWorksheet('Autos');
+
+
+    //Add Row and formatting
+    let titleRow = worksheet.addRow([title]);
+    titleRow.font = { name: 'Arial', family: 4, size: 16, bold: true }
+    worksheet.addRow([]);
+
+    worksheet.mergeCells('A1:D2');
+
+
+    //Blank Row 
+    worksheet.addRow([]);
+
+    //Add Header Row
+    let headerRow = worksheet.addRow(header);
+    
+    // Cell Style : Fill and Border
+    headerRow.eachCell((cell, number) => {
+      cell.fill = {
+        type: 'pattern',
+        pattern: 'solid',
+        fgColor: { argb: 'FFFFFF00' },
+        bgColor: { argb: 'FF0000FF' }
+      }
+      cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
+    })
+    // worksheet.addRows(data);
+
+
+    // Add Data and Conditional Formatting
+    dataTable.forEach(d => {
+      let row = worksheet.addRow(d);
+      let qty = row.getCell(5);
+      let color = 'FF9999';
+      if (+qty.value < 500) {
+        color = 'FF9999'
+      }
+
+      
+    }
+
+    );
+
+    worksheet.getColumn(3).width = 30;
+    worksheet.getColumn(4).width = 30;
+    worksheet.addRow([]);
+
+    //Generate Excel File with given name
+    workbook.xlsx.writeBuffer().then((dataTable) => {
+      let blob = new Blob([dataTable], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, 'reporte.xlsx');
+    })
+
+  }
 }
